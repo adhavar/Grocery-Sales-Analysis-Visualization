7aa6b79d7aa47e48a1de131cbf8f767f77af4801
adhavar <adhamaryz.vargas@gmail.com>
2026-08-04 09:07:27 -0700

    Table joins and column selection

---
+++ sql/3_join_and_selection.sql
@@ -0,0 +1,32 @@
+/* In this step, I select the columns needed to meet the Business Objectives. */
+
+CREATE TABLE table_analysis AS
+SELECT
+    pd."ProductName", 
+    pd."Class", 
+    pd."Resistant", 
+    pd."IsAllergic", 
+    pd."VitalityDays",
+    /* In the original sales table, the TotalPrice column only has the value of 0.
+    Because of the incorrect value, I created a new column. */
+    CAST(
+        (sl."Quantity" * pd."Price") - sl."Discount" 
+        AS DECIMAL(10,2)) AS TotalPrice, 
+    cs."CustomerID",
+    /* To easier identify and present the top employees, a full name column needs to be created. 
+    It can also be donde using || */
+    CONCAT(emp."FirstName", 
+        ' ', 
+        SUBSTR(emp."MiddleInitial", 1, 1),
+        '. ', 
+        emp."LastName") AS EmployeeName,
+    ct."CityName", 
+    cr."CountryName"
+
+FROM sales AS sl 
+    LEFT JOIN employees AS emp ON sl."SalesPersonID" = emp."EmployeeID" 
+    LEFT JOIN customers AS cs ON sl."CustomerID" = cs."CustomerID" 
+    LEFT JOIN cities AS ct ON cs."CityID" = ct."CityID" 
+    LEFT JOIN countries AS cr ON ct."CountryID" = cr."CountryID" 
+    LEFT JOIN products AS pd ON sl."ProductID" = pd."ProductID" 
+    LEFT JOIN categories AS cat ON pd."CategoryID" = cat."CategoryID"
